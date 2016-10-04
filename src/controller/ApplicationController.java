4ec48087984e5c11e23bced37e48583e269554ac
kopaka7 <Naren@lawn-128-61-23-188.lawn.gatech.edu>
2016-10-04 15:48:58 -0400

    Added java docs

--- src/controller/ApplicationController.java
+++ src/controller/ApplicationController.java
@@ -11,7 +11,9 @@ import javafx.scene.text.TextAlignment;
 import model.Model;
 import model.User;
 
-
+/**
+* Main class for the application controller
+*/
 public class ApplicationController {
 
     @FXML
@@ -27,21 +29,34 @@ public class ApplicationController {
 
     private User activeUser;
 
+    /**
+    * initializes the application state
+    */
     @FXML
     private void initialize() {
         welcomeMessage.setText("Welcome!");
         welcomeMessage.setTextAlignment(TextAlignment.CENTER);
         welcomeHBox.setAlignment(Pos.CENTER);
     }
-
+    /**
+    * Setter method for the active user
+    * @param user the user being passed in as an active user
+    */
     public void setActiveUser(User user) {
         activeUser = user;
         welcomeMessage.setText("Welcome " + activeUser.getUserName() + "!");
     }
+    /**
+    * Getter method for the active user
+    * @return the currently active user
+    */
     public User getActiveUser() {
         return activeUser;
     }
-
+    /**
+    * Setter method instantiates the main application
+    * @param mainApplication the instance of the main application class
+    */
     public void setMainApp(MainApplication mainApplication) {
         this.mainApplication = mainApplication;
     }
@@ -49,13 +64,17 @@ public class ApplicationController {
     public void focus() {
         anchorPane.requestFocus();
     }
-
+    /**
+    * Method to sign out the user
+    */
     @FXML
     private void handleSignOutPressed() {
         Model.getInstance().setLoggedUser(new User());
         mainApplication.initMainLayout();
     }
-
+    /**
+    * Method to display the user profile when profile is pressed
+    */
     @FXML
     private void handleMyProfilePressed() {
         mainApplication.showProfileScreen();
